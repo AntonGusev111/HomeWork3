cbc9691e03d0e08194e5499f2d0caebfec508974
Admin01001 <GusevAD111@gmail.com>
2021-10-25 00:51:47 +0300

    finish home work

--- main.py
+++ main.py
@@ -27,14 +27,14 @@ class YaUploader:
         return os.path.basename(self.path_to_file)
 
     def _upload_link(self):
-        url=f"https://cloud-api.yandex.net/v1/disk/resources/upload"
-        param={"path": self._open_file(),'overwrite': "true"}
-        response = requests.get(url, headers=self._headers(), params=param)
+        url = f"https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {"path": self._open_file(),'overwrite': "true"}
+        response = requests.get(url, headers=self._headers(), params=params)
         return response.json()
 
     def upload(self):
-        response= requests.put(self._upload_link().get('href',''), data=open(self.path_to_file,'rb'))
-        if response.status_code==201:
+        response = requests.put(self._upload_link().get('href',''), data=open(self.path_to_file,'rb'))
+        if response.status_code == 201:
             return ('File upload!!!')
         else:
             return (f"Oops, status code is {response.status_code}")
@@ -44,11 +44,11 @@ class YaUploader:
 
 def stac_questions():
     url = 'https://api.stackexchange.com/2.3/questions'
-    params={'order': 'desc','min': (round(time.time())-172800), 'max': round(time.time()), 'sort': 'activity', 'tagged': 'python', 'site': 'stackoverflow' }
+    params = {'order': 'desc','min': (round(time.time())-172800), 'max': round(time.time()), 'sort': 'activity', 'tagged': 'python', 'site': 'stackoverflow' }
     response = requests.get(url, params=params)
-    questions_dict= {}
+    questions_dict = {}
     for i in range(len(response.json()['items'])):
-        questions_dict[response.json()['items'][i]['owner']['display_name']]=response.json()['items'][i]['title']
+        questions_dict[response.json()['items'][i]['owner']['display_name']] = response.json()['items'][i]['title']
     return(questions_dict)
 
 
